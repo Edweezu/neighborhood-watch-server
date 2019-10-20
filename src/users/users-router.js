d6cc2c29def38f5d9d7f2adfbde10971a99a5b7e
Edwin Qiu <edwinqiu93@gmail.com>
2019-10-20 01:53:36 -0700

    testing deploy

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -14,7 +14,7 @@ UsersRouter
     // .all(requireAuth)
     .get((req, res, next) => {
         let db = req.app.get('db')
-
+        console.log('hi', db)
         return UsersService.getAllUsers(db)
             .then(users => {
                 console.log('response users', users)
