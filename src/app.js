b9bc1326c58f1ed6bc797b9fa110348de0733f6c
Edwin Qiu <edwinqiu93@gmail.com>
2019-11-04 03:46:01 -0800

    testing

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const PlacesRouter = require('./places/places-router')
 const CommentsRouter = require('./comments/comments-router')
 const { cloudinaryConfig, uploader } = require('./cloudinaryConfig')
 const { urlencoded, json } = require('body-parser')
+const path = require('path')
 
 
 const app = express()
@@ -33,6 +34,17 @@ app.use('/api/users', UsersRouter)
 app.use('/api/posts', PostsRouter)
 app.use('/api/places', PlacesRouter)
 app.use('/api/comments', CommentsRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/api/posts', express.static("build"))
+    app.use('/api/users', express.static("build"))
+    app.use('/api/places', express.static("build"))
+    app.use('/api/comments', express.static("build"))
+
+    app.get("*", (req, res) => {
+        res.sendfile(path.resolve(__dirname,  "build", "index.html"));
+    });
+}
 
 app.use(ErrorHandler)
 
